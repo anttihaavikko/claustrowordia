f1c4f43d16b37b698a2a43e3de3fb1341427f9bd
Antti Haavikko <antti.haavikko@gmail.com>
2022-04-03 20:37:55 +0300

    Update build pushing script

--- pushbuilds.sh
+++ pushbuilds.sh
@@ -4,15 +4,15 @@ NC='\033[0m' # No Color
 echo " "
 
 echo "${COLOR}Pushing build for Windows${NC}"
-butler push Builds/win anttihaavikko/replace:win --fix-permissions
+butler push Builds/win anttihaavikko/claustrowordia:win --fix-permissions
 
 echo "${COLOR}Pushing build for OSX${NC}"
-butler push Builds/osx anttihaavikko/replace:osx --fix-permissions
+butler push Builds/osx anttihaavikko/claustrowordia:osx --fix-permissions
 
 echo "${COLOR}Pushing build for Linux${NC}"
-butler push Builds/linux anttihaavikko/replace:linux
+butler push Builds/linux anttihaavikko/claustrowordia:linux
 
-echo "${COLOR}Copying html5 files over to correct path"
-cp -a Builds/webgl/html5/Build/. Builds/html5/Build
+#echo "${COLOR}Copying html5 files over to correct path"
+#cp -a Builds/webgl/html5/Build/. Builds/html5/Build
 echo "${COLOR}Pushing build for HTML5${NC}"
-butler push Builds/html5 anttihaavikko/replace:html5
+butler push Builds/html5 anttihaavikko/claustrowordia:html5
